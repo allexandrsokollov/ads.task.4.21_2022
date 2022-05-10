a04e3a60a679fb5f06a8b5ad540aa9c5bc42a9ae
Alexandr <allexandersokollov@gmail.com>
2022-05-10 09:05:02 +0300

    added getRandomIntArray

--- src/main/java/vsu/cs/sokolov/Main.java
+++ src/main/java/vsu/cs/sokolov/Main.java
@@ -1,7 +1,25 @@
 package vsu.cs.sokolov;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 
+        int[] arr = getRandomIntArray(20);
+
+        for (int value : arr) {
+            System.out.println(value);
+        }
+    }
+    static int[] getRandomIntArray(int lengthOfArray) {
+        int[] resultArray = new int[lengthOfArray];
+
+        Random random = new Random();
+        for (int i = 0; i < lengthOfArray; i++) {
+            resultArray[i] = random.nextInt(10);
+        }
+
+        return resultArray;
     }
 }
+
