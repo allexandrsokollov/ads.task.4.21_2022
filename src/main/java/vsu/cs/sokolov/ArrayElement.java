9fc83113d67c96dff73d7549bedbfc8dd0e2f76e
Alexandr <allexandersokollov@gmail.com>
2022-05-10 09:08:47 +0300

    added ArrayElement.java

---
+++ src/main/java/vsu/cs/sokolov/ArrayElement.java
@@ -0,0 +1,28 @@
+package vsu.cs.sokolov;
+
+public class ArrayElement {
+    private final int value;
+    private final int initialIndex;
+    private  int sortedIndex;
+
+    public ArrayElement(int value, int initialIndex) {
+        this.value = value;
+        this.initialIndex = initialIndex;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int getInitialIndex() {
+        return initialIndex;
+    }
+
+    public void setSortedIndex(int sortedIndex) {
+        this.sortedIndex = sortedIndex;
+    }
+
+    public int getSortedIndex() {
+        return sortedIndex;
+    }
+}
