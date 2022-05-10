6f41e423574e0116a84e80220776ff7ebc847d6a
Alexandr <allexandersokollov@gmail.com>
2022-05-10 13:35:39 +0300

    deleted useless code

--- src/main/java/vsu/cs/sokolov/ArrayElement.java
+++ src/main/java/vsu/cs/sokolov/ArrayElement.java
@@ -1,9 +1,5 @@
 package vsu.cs.sokolov;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Comparator;
-
 public class ArrayElement {
     private final int value;
     private final int initialIndex;
@@ -18,10 +14,6 @@ public class ArrayElement {
         this.value = element.value;
         this.sortedIndex = element.sortedIndex;
         this.initialIndex = element.initialIndex;
-    }
-
-    public static void sortElements(ArrayElement[] elements, Comparator<ArrayElement> comparator) {
-        Arrays.sort(elements, comparator);
     }
 
     public int getValue() {
