39e3fd272919bbe09529e9aea933509c814ac5fd
Alexandr <allexandersokollov@gmail.com>
2022-05-10 09:23:35 +0300

    added comparators in Main.java 1. Sorting via value 2. Sorting via initialIndex

--- src/main/java/vsu/cs/sokolov/ArrayElement.java
+++ src/main/java/vsu/cs/sokolov/ArrayElement.java
@@ -1,5 +1,9 @@
 package vsu.cs.sokolov;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class ArrayElement {
     private final int value;
     private final int initialIndex;
@@ -8,6 +12,10 @@ public class ArrayElement {
     public ArrayElement(int value, int initialIndex) {
         this.value = value;
         this.initialIndex = initialIndex;
+    }
+
+    public static void sortElements(ArrayElement[] elements, Comparator<ArrayElement> comparator) {
+        Arrays.sort(elements, comparator);
     }
 
     public int getValue() {
--- src/main/java/vsu/cs/sokolov/Main.java
+++ src/main/java/vsu/cs/sokolov/Main.java
@@ -1,15 +1,41 @@
 package vsu.cs.sokolov;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        int[] arr = getRandomIntArray(20);
+        Scanner in = new Scanner(System.in);
+        int arrayLength;
+        System.out.print("Enter array length: ");
+        arrayLength = in.nextInt();
+        System.out.println();
 
-        for (int value : arr) {
-            System.out.println(value);
+        int[] array = getRandomIntArray(arrayLength);
+        ArrayElement[] elementsArray = new ArrayElement[arrayLength];
+
+        for (int i = 0; i < arrayLength; i++) {
+            elementsArray[i] = new ArrayElement(array[i], i);
         }
+
+        Comparator<ArrayElement> compareViaValue = Comparator.comparingInt(ArrayElement::getValue);
+        Arrays.sort(elementsArray, compareViaValue);
+
+        for (int i = 0; i < arrayLength; i++) {
+            elementsArray[i].setSortedIndex(i);
+        }
+
+        Comparator<ArrayElement> compareViaInitialIndex = Comparator.comparingInt(ArrayElement::getInitialIndex);
+        Arrays.sort(elementsArray, compareViaInitialIndex);
+
+
+        for (ArrayElement element : elementsArray) {
+            System.out.println(element.getValue() + "  " + element.getInitialIndex() + " " + element.getSortedIndex());
+        }
+
     }
     static int[] getRandomIntArray(int lengthOfArray) {
         int[] resultArray = new int[lengthOfArray];
